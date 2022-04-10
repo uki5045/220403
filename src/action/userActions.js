a6a4d4a7c424ee7ec6435ff787d15c7a81162062
hanjinwook <uki5045@gmail.com>
2022-04-10 15:02:51 +0900

    redux #2(컨스텐츠, 액션스)

---
+++ src/action/userActions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT
+} from '../constants/userConstants'
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        const {data} = await axios.post('http://localhost:8000/api/users/login', {email, password})
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+    } catch (err) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+                err.response && err.response.data.message
+                    ? err.response.data.message
+                    : err.message
+        })
+    }
+}
